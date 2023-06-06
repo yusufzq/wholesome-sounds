b0059f095a321bff8066f17751aa9a7948792765
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-06 18:08:47 +0100

    port upLoader into component, format

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -2,37 +2,7 @@
 	<section class='container mx-auto mt-6'>
 		<div class='md:grid md:grid-cols-3 md:gap-4'>
 			<div class='col-span-1'>
-				<div class='bg-white rounded border border-gray-200 relative flex flex-col'>
-					<div class='px-6 pt-6 pb-5 font-bold border-b border-gray-200'>
-						<span class='card-title'>UpLoad</span>
-						<i class='fas fa-upload float-right text-green-400 text-2xl'></i>
-					</div>
-					<div class='p-6'>
-						<div
-							class='w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid'>
-							<h5>Drop Files</h5>
-						</div>
-						<hr class='my-6' />
-						<div class='mb-4'>
-							<div class='font-bold text-sm'>sound.mp3</div>
-							<div class='flex h-4 overflow-hidden bg-gray-200 rounded'>
-								<div class='transition-all progress-bar bg-blue-400' style='width: 75%'></div>
-							</div>
-						</div>
-						<div class='mb-4'>
-							<div class='font-bold text-sm'>sound.mp3</div>
-							<div class='flex h-4 overflow-hidden bg-gray-200 rounded'>
-								<div class='transition-all progress-bar bg-blue-400' style='width: 35%'></div>
-							</div>
-						</div>
-						<div class='mb-4'>
-							<div class='font-bold text-sm'>sound.mp3</div>
-							<div class='flex h-4 overflow-hidden bg-gray-200 rounded'>
-								<div class='transition-all progress-bar bg-blue-400' style='width: 55%'></div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<UpLoader />
 			</div>
 			<div class='col-span-2'>
 				<div class='bg-white rounded border border-gray-200 relative flex flex-col'>
@@ -55,15 +25,11 @@
 								<form>
 									<div class='mb-3'>
 										<label class='inline-block mb-2'>Sound Title</label>
-										<input type='text'
-											class='block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded'
-											placeholder='title' />
+										<input type='text' class='block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded' placeholder='title' />
 									</div>
 									<div class='mb-3'>
 										<label class='inline-block mb-2'>Genre</label>
-										<input type='text'
-											class='block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded'
-											placeholder='genre' />
+										<input type='text' class='block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded' placeholder='genre' />
 									</div>
 									<button type='submit' class='py-1.5 px-3 rounded text-white bg-green-600'>
 										Submit
@@ -126,18 +92,10 @@
 </template>
 
 <script>
-	// import useUserStore from '@/stores/user';
+	import UpLoader from '@/components/UpLoader.vue';
 
 	export default {
-		name: 'manage'
-		// beforeRouteEnter(to, from, next) {
-		// 	const userStore = useUserStore();
-			
-		// 	if (userStore.loggedIn) {
-		// 		next();
-		// 	} else {
-		// 		next({name: 'home'});	
-		// 	};
-		// }
+		name: 'manage',
+		components: { UpLoader }
 	};
 </script>
