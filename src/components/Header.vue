5c8a46232e86efeb653d6e488d88272a317cb115
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-06 16:54:30 +0100

    bind RouterLink links with route names instead of paths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
 	<header id='header' class='bg-gray-700'>
 		<nav class='container mx-auto flex justify-between items-center py-5 px-4'>
-			<RouterLink to='/' class='text-white font-bold uppercase text-2xl mr-4' exact-active-class='inActive'>WholeSome Sounds</RouterLink>
+			<RouterLink :to='{name: "home"}' class='text-white font-bold uppercase text-2xl mr-4' exact-active-class='inActive'>WholeSome Sounds</RouterLink>
 			<div class='flex items-center'>
 				<ul class='flex flex-row mt-1'>
 					<template v-if='userStore.loggedIn'>
 						<li>
-							<RouterLink to='/manage' class='px-2 text-white'>Manage</RouterLink>
+							<RouterLink :to='{name: "manage"}' class='px-2 text-white'>Manage</RouterLink>
 						</li>
 						<li>
 							<a href='#' class='px-2 text-white' @click.prevent='userStore.logOut'>Log Out</a>
