fab7d3ae5247d7acbc976351dc28d0fac63649e6
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-10 16:54:02 +0100

    port soundItem into own component, query FireStore for sounds for homePage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-		<section class='mb-8 py-20 text-white text-center relative'>
+	<section class='mb-8 py-20 text-white text-center relative'>
 		<div style='background-image: url(images/header.png)' class='absolute inset-0 w-full h-full bg-contain introduction-background'></div>
 		<div class='container mx-auto'>
 			<div class='text-white main-header-content'>
@@ -21,163 +21,30 @@
 				<i class='fa fa-headphones-alt float-right text-green-400 text-xl'></i>
 			</div>
 			<ol id='playlist'>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
-				<li class='flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50'>
-					<div>
-						<a href='#' class='font-bold block text-gray-600'>Sound</a>
-						<span class='text-gray-500 text-sm'>Author</span>
-					</div>
-					<div class='text-gray-600 text-lg'>
-						<span class='comments'>
-							<i class='fa fa-comments text-gray-600'></i>
-							19
-						</span>
-					</div>
-				</li>
+				<SoundItem v-for='sound in sounds' :key='sound.documentID' :sound='sound' />
 			</ol>
 		</div>
 	</section>
 </template>
+
+<script>
+	import { soundsCollection } from '@/includes/fireBase';
+	import SoundItem from '@/components/SoundItem.vue';
+
+	export default {
+		name: 'Home',
+		components: { SoundItem },
+		data() {
+			return {
+				sounds: []
+			};
+		},
+		async created() {
+			const snapShots = await soundsCollection.get();
+
+			snapShots.forEach(document => {
+				this.sounds.push({documentID: document.id, ...document.data()});
+			});
+		}
+	};
+</script>
