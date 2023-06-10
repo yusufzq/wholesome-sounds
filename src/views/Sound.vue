4572f08c9e6629cf35bee0419353dca07d12850b
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-11 00:31:23 +0100

    update comment-count to/from FireStore

--- src/views/Sound.vue
+++ src/views/Sound.vue
@@ -14,7 +14,7 @@
 	<section class='container mx-auto mt-6'>
 		<div class='bg-white rounded border border-gray-200 relative flex flex-col'>
 			<div class='px-6 pt-6 pb-5 font-bold border-b border-gray-200'>
-				<span class='card-title'>Comments (19)</span>
+				<span class='card-title'>Comments ({{ sound.commentCount }})</span>
 				<i class='fa fa-comments float-right text-green-400 text-2xl'></i>
 			</div>
 			<div class='p-6'>
@@ -113,6 +113,8 @@
 				};
 
 				await commentsCollection.add(comment);
+				this.sound.commentCount++;
+				await soundsCollection.doc(this.$route.params.ID).update({commentCount: this.sound.commentCount});
 				this.getComments();
 
 				this.pending = false;
