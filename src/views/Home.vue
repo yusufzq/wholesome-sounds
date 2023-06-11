74c8521fbaffc14383060c156f9bed86f39cce42
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-11 17:40:20 +0100

    allow icon directive to have binding modifiers for right-floating and yellow colour

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
 		</section>
 		<section class='container mx-auto'>
 			<div class='bg-white rounded border border-gray-200 relative flex flex-col'>
-				<div class='px-6 pt-6 pb-5 font-bold border-b border-gray-200' v-icon='"headphones-alt"'>
+				<div class='px-6 pt-6 pb-5 font-bold border-b border-gray-200' v-icon.right.yellow='"headphones-alt"'>
 					<span class='card-title'>Sounds</span>
 				</div>
 				<ol id='playlist'>
