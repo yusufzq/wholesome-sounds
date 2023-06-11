b6ef6d34bb0464e5b07cc78622644c8d0c2c23ae
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-11 17:16:03 +0100

    allow icon directive to have binding argument as full value

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,6 +1,10 @@
 const IconDirective = {
 	beforeMount(el, binding) {
-		const iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`;
+		let iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`;
+
+		if (binding.arg === 'full') {
+			iconClass = binding.value;
+		};
 
 		el.innerHTML += `<i class='${iconClass}'></i>`;
 	}
