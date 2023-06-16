774834c506d006ae72e798be5202ba02b7e70002
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-06-16 10:26:39 +0100

    update translations, add horizontal padding to hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,10 @@
 			<div class='container mx-auto'>
 				<div class='text-white main-header-content'>
 					<h1 class='font-bold text-5xl mb-5'>{{ $t('home.heading') }}</h1>
-					<p class='w-full md:w-8/12 mx-auto'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-						et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-						suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-						pulvinar, fringilla lorem eget, ullamcorper urna.
-					</p>
+					<p class='w-full md:w-8/12 mx-auto'>{{ $t('home.subHeading') }}</p>
 				</div>
 			</div>
-			<img src='/images/hero.png' class='relative block mx-auto mt-5 -mb-20 w-auto max-w-full pb-8' />
+			<img src='/images/hero.png' class='relative block mx-auto mt-5 -mb-20 w-auto max-w-full pb-8 px-4' />
 		</section>
 		<section class='container mx-auto'>
 			<div class='bg-white rounded border border-gray-200 relative flex flex-col'>
